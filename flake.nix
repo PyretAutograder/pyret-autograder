0d95317e155b3f11a3ba1d0c3dba458d390114c2
IRONM00N <me@ironmoon.dev>
2025-05-23 14:33:16 -0400

    add flake since Makefile depends on /bin/bash :(

---
+++ flake.nix
@@ -0,0 +1,43 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      systems,
+    }:
+    let
+      eachSystem = f: nixpkgs.lib.genAttrs (import systems) (s: f (import nixpkgs { system = s; }));
+    in
+    {
+      packages = eachSystem (pkgs: {
+        default = pkgs.buildFHSEnv {
+          name = "pyret-fhs-env";
+
+          targetPkgs =
+            pkgs: with pkgs; [
+              nodejs_22
+              git
+              gnumake
+              bash
+              curl
+              python3
+            ];
+
+          runScript = "bash";
+        };
+      });
+
+      devShells = eachSystem (pkgs: {
+        default = pkgs.mkShell {
+          packages = with pkgs; [
+            nodejs_22
+            gnumake
+          ];
+        };
+      });
+    };
+}
