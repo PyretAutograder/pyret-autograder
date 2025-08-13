2d46c653ef9e15c5f6ca0345ae3c34396e83f201
IRONM00N <me@ironmoon.dev>
2025-08-13 18:16:23 -0400

    add support to use custom fd for output

--- src/tools/io.js
+++ src/tools/io.js
@@ -4,10 +4,16 @@
   provides: {
     values: {
       "get-stdin": ["arrow", [], "String"],
+      "send-final": ["arrow", ["String"], "Nothing"],
     },
   },
-  nativeRequires: [],
-  theModule: function (runtime, _namespace, _url) {
+  nativeRequires: ["fs/promises"],
+  /**
+   * @param {import('fs/promises')} fs
+   */
+  theModule: function (runtime, _namespace, _url, fs) {
+    "use strict"
+
     let input = "";
     let eof = false;
 
@@ -28,8 +34,29 @@
       return runtime.makeString(input);
     }
 
+    /**
+      * @param {string} s
+      */
+    function sendFinal(s) {
+      runtime.checkArity(1, arguments, "send-final", false);
+      runtime.checkString(s);
+
+      const fd = (() => {
+        const v = process.env.PA_RESULT_FD;
+        return (v != null && /^\d+$/.test(v)) ? Number(v) : 3;
+      })();
+
+      return runtime.await((async () => {
+        const line = s.endsWith("\n") ? s : s + "\n";
+        // @ts-ignore: type definitions are wrong
+        await fs.writeFile(fd, line);
+        return runtime.nothing;
+      })());
+    }
+
     return runtime.makeModuleReturn({
       "get-stdin": runtime.makeFunction(getStdin, "get-stdin"),
+      "send-final": runtime.makeFunction(sendFinal, "send-final"),
     }, {});
   }
 })
